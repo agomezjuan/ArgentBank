4adfb8b551da81c6fe2c6b2f970d460ae5009c2d
Alberto Gómez Juan <agomezjuan@hotmail.com>
2023-01-15 13:05:38 +0100

    refactor: :recycle: change a tag to router Link (#7)
    Great work!!

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const Logo = () => {
   return (
-    <a className="main-nav-logo" href="./index.html">
+    <Link className="main-nav-logo" to="/">
       <img className="main-nav-logo-image" src="./img/argentBankLogo.png" alt="Argent Bank Logo" />
       <h1 className="sr-only">Argent Bank</h1>
-    </a>
+    </Link>
   );
 };
 export default Logo;
